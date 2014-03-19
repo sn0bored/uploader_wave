9b17e8f585f25998a88d3bee519bc7ab2b9c7600
Daniel Willems <dmkwillems@gmail.com>
2014-03-19 14:45:21 -0400

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Upwave::Application.config.secret_token = 'a035cf6bf4a056addef011f76bf30c4190d179e5d9625c3322a23e498fe942515dce8d92e4db3b1815994b2933ffea12b92d6e1974371a896d44ef0134fff74c'
