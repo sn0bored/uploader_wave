d0115b9513e98de6536df1fb57b836fd48e63606
Daniel Willems <dmkwillems@gmail.com>
2014-03-19 15:52:43 -0400

    test for UploadFile model#exists

--- spec/models/upload_file_spec.rb
+++ spec/models/upload_file_spec.rb
@@ -1,5 +1,19 @@
 require 'spec_helper'
 
 describe UploadFile do
-  pending "add some examples to (or delete) #{__FILE__}"
+	
+  it "model the exists" do
+    upload_file = UploadFile.create
+    upload_file.should be_a(UploadFile)
+  end
+
+  it "should uploaded a file" do
+  	# upload file
+  end
+
+  it "should not upload a file if the a file with the exact contents exist" do
+  	#uploadfile
+  	#try again
+  end
+
 end
