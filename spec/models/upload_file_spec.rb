7d9859db5efb8b523c1e6ec04d368b78e416043e
Daniel Willems <dmkwillems@gmail.com>
2014-03-19 16:12:32 -0400

    config of carrierwave for test

--- spec/models/upload_file_spec.rb
+++ spec/models/upload_file_spec.rb
@@ -1,6 +1,19 @@
 require 'spec_helper'
 
 describe UploadFile do
+	include CarrierWave::Test::Matchers
+
+	before do
+		# UpwaveUploader.enable_processing = true
+		# @uploader = UpwaveUploader.new(@upload_file, :content)
+    # @uploader.store!(File.open())
+	end
+
+	after do
+    # MyUploader.enable_processing = false
+    # @uploader.remove!
+  end
+
 	
   it "model the exists" do
     upload_file = UploadFile.create
@@ -8,7 +21,8 @@ describe UploadFile do
   end
 
   it "should uploaded a file" do
-  	# upload file
+  	# upload_file = UploadFile.create
+  	# upload_file.content = 
   end
 
   it "should not upload a file if the a file with the exact contents exist" do
