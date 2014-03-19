73dcbf63fcf0ddd98e57931ddc6914fa6e39d162
Daniel Willems <dmkwillems@gmail.com>
2014-03-19 16:34:33 -0400

    exists and uploader specs in seperate describe blocks. uploader fails

--- config/carrierwave.rb
+++ config/carrierwave.rb
@@ -1,6 +1,6 @@
-# if Rails.env.test? || Rails.env.development?
-#   CarrierWave.configure do |config|
-#     config.storage = :file
-#     config.enable_processing = true
-#  	end
-# end
+if Rails.env.test? || Rails.env.development?
+  CarrierWave.configure do |config|
+    config.storage = :file
+    config.enable_processing = true
+ 	end
+end
--- spec/models/upload_file_spec.rb
+++ spec/models/upload_file_spec.rb
@@ -3,31 +3,41 @@ require 'spec_helper'
 describe UploadFile do
 	include CarrierWave::Test::Matchers
 
-	before do
-		# UpwaveUploader.enable_processing = true
-		# @uploader = UpwaveUploader.new(@upload_file, :content)
-    # @uploader.store!(File.open())
+	describe "model" do
+		
+		it "exists" do
+	    upload_file = UploadFile.create
+	    upload_file.should be_a(UploadFile)
+	  end
+  
+  end
+
+
+  describe "uploader" do
+
+		before do
+			dir = Rails.root
+			test_path = File.join(dir, "spec/test.txt")
+			UpwaveUploader.enable_processing = true
+			@uploader = UpwaveUploader.new(@upload_file, :content)
+	    @uploader.store!(File.open(test_path))
+		end
+
+		after do
+	    MyUploader.enable_processing = false
+	    @uploader.remove!
+	  end
+
+	  it "should uploaded a file" do
+	  	# upload_file = UploadFile.create
+	  	# upload_file.content = 
+	  end
+
+	  it "should not upload a file if the a file with the exact contents exist" do
+	  	#uploadfile
+	  	#try again
+	  end
+
 	end
-
-	after do
-    # MyUploader.enable_processing = false
-    # @uploader.remove!
-  end
-
-	
-  it "model the exists" do
-    upload_file = UploadFile.create
-    upload_file.should be_a(UploadFile)
-  end
-
-  it "should uploaded a file" do
-  	# upload_file = UploadFile.create
-  	# upload_file.content = 
-  end
-
-  it "should not upload a file if the a file with the exact contents exist" do
-  	#uploadfile
-  	#try again
-  end
 
 end
