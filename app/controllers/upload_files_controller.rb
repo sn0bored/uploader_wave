425364313a05c130a43a12930145d80104f6634d
Daniel Willems <dmkwillems@gmail.com>
2014-03-19 20:08:24 -0400

    basic wireframe for file creation

--- app/controllers/upload_files_controller.rb
+++ app/controllers/upload_files_controller.rb
@@ -1,12 +1,15 @@
 class UploadFilesController < ApplicationController
 
 	def index
+		@uploaded_files = UploadFile.all
 	end
 
-	# def show
-	# end
+	def show
+		@uploaded_file = UploadFile.find(params[:id])
+	end
 
-	# def create
-	# end
+	def create
+		# @uploaded_file = UploadFile.new
+	end
 
 end
